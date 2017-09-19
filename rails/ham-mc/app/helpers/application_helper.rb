46fa88d40108967292b83b6a17f087562c85cded
Kai Marshland <kaimarshland@gmail.com>
2017-09-18 17:35:23 -0700

    Wrote part 6

--- rails/ham-mc/app/helpers/application_helper.rb
+++ rails/ham-mc/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def menu_item(name, path)
+    css_class = ''
+
+    if current_page? path
+      css_class = 'active'
+    end
+
+    "<li class=\"#{css_class}\">#{link_to name, path}</li>".html_safe
+  end
+
 end
